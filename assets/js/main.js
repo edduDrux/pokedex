c7df90bf12d5b5a0b4f523544bccb1115d803c9e
Eduardo Sartori <eddudrux@gmail.com>
2024-12-23 07:21:19 -0300

    commit padrão

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,34 @@
-console.log('Hello, world!');
+const pokemonList = document.getElementById('pokemonList')
+const loadMore = document.getElementById('loadMore')
+const limit = 5
+let offfset = 0
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="details">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+
+                <img src="${pokemon.photo}"
+                     alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+
+function loadPokemonItens (offset, limit) {
+    pokeApi.getPokemons().then((pokemons = []) => {
+        const newHtml = pokemons.map(convertPokemonToLi).join('')
+        pokemonList.innerHTML = newHtml
+    })
+}
+
+loadMore.addEventListener('click', () => {
+    loadPokemonItens()
+})
